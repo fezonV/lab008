8dfdab360d1d22e55328da585a43bbb1da641ebc
Алексей Анисимов <mr.razor2706@gmail.com>
2024-10-04 13:11:44 +0300

    Изменил код под задание 1

--- hello.pas
+++ hello.pas
@@ -1,3 +1,9 @@
 ﻿begin
-  Print('Привет, мир!');
+  var hour := ReadInteger('Введите час: ');
+  Assert((hour > 0) and (hour < 23));
+  case hour of
+    4..10: Println('Доброе утро, мир!');
+    11..16: Println('Добрый вечер, мир!');
+    17..22: Println('Доброй ночи, мир!');
+  end;
 end.
