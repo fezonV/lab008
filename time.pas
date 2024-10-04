86345211924e75556c8a4afe8e56adaca8d629f3
Алексей Анисимов <mr.razor2706@gmail.com>
2024-10-04 13:16:06 +0300

    Решение задания 2

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var year := ReadInteger('Введите год: ');
+  if (year mod 4 = 0) and (year mod 100 <> 0) and (year mod 400 <> 0) then
+    Println('Год високосный.')
+  else
+    Println('Год не високосный.');
+end.
